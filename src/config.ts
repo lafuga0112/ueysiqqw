76ecece38282bc9abf66f7e2a68da3210e901eb3
lafuga0112 <50000949+lafuga0112@users.noreply.github.com>
2025-01-29 21:12:26 -0600

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,6 @@
 //export const HOST = 'https://callhandler.xsebasthian.workers.dev'
-export const HOST = 'https://websockets-9jdae.ondigitalocean.app'
+export const HOST = 'https://clownfish-app-fbsnl.ondigitalocean.app'
 export const API_URL = 'https://seashell-app-pki2c.ondigitalocean.app'
 // export const DBURL = 'https://blacklist13.altervista.org/calls_db.php'
-export const websockUrl = 'https://websockets-9jdae.ondigitalocean.app/webhook'
+export const websockUrl = 'https://clownfish-app-fbsnl.ondigitalocean.app/webhook'
 export const campaignsUrl = 'https://mrbots.pro/api/obtener_scripts/'
-
-
-
